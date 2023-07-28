d6e0d1a492ba02d74ad4f398f7aaf3b591f5c506
RodriDev23 <rodrigoprogramador23@outlook.com>
2023-07-28 17:05:30 -0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int generateNumber() {
+   return rand();
+}
+std::string passowrdGenerator(int length){
+std::string password = "";
+std::string allLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+for(int i = 0; i < length; i++){
+ int randomIndex= generateNumber() % allLetters.length();
+ password.push_back(allLetters[randomIndex]);  
+}
+return password;
+
+}
+int main(){
+    std::string newPassword = passowrdGenerator(12);
+    std::cout<<"your new password is "<<newPassword << std::endl;
+    return 0;
+}
+
